9b65f26162d6b691469bea899fc283e2c11d0288
Lukas Panni <lukas.panni@outlook.de>
2022-02-13 19:02:46 +0100

    Add creation option

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import path from 'path';
 import fs from 'fs';
 import commandLineArgs from 'command-line-args';
 import commandLineUsage from 'command-line-usage';
-import packageData from '../package.json';
+const packageData = require('../package.json');
 
 const optionDefinitions = [
   {
@@ -19,9 +19,21 @@ const optionDefinitions = [
     type: Boolean
   },
   {
+    name: 'force',
+    description: 'Force execution of a command',
+    alias: 'f',
+    type: Boolean
+  },
+  {
     name: 'root',
     description: 'The root directory of the cordova-project',
     alias: 'r',
+    type: String
+  },
+  {
+    name: 'create',
+    description: 'Create a new config with specified name based on the default cordova.config',
+    alias: 'c',
     type: String
   }
 ];
@@ -29,6 +41,22 @@ const optionDefinitions = [
 const main = (callArguments: commandLineArgs.CommandLineOptions): void => {
   const continueExecution = handleMetaOptions(callArguments);
   if (!continueExecution) return;
+
+  const configRoot = getCordovaProjectRoot(callArguments['root']);
+  const mainConfig = path.join(configRoot, 'config.xml');
+  // get all configFiles (config*.xml)
+  const configFiles = fs.readdirSync(configRoot).filter((file) => file.startsWith('config') && file.endsWith('.xml'));
+
+  if (callArguments['create'] !== undefined && callArguments['create'].length !== 0) {
+    const configFilename = 'config.' + callArguments['create'] + '.xml';
+    if (!callArguments['force'] && configFiles.includes(configFilename)) {
+      console.error(`Error: the config file ${configFilename} already exists. To force creation use -f`);
+      return;
+    }
+    fs.copyFileSync(mainConfig, path.join(configRoot, configFilename));
+    console.log(`Successfully created config file ${configFilename}`);
+    return;
+  }
 };
 
 const handleMetaOptions = (callArguments: commandLineArgs.CommandLineOptions): boolean => {
@@ -52,10 +80,11 @@ const handleMetaOptions = (callArguments: commandLineArgs.CommandLineOptions): b
   return true;
 };
 
-const getCordovaProjectRoot = (): string | undefined => {
+const getCordovaProjectRoot = (root?: string): string => {
+  if (root && fs.existsSync(path.join(root, 'config.xml'))) return root;
   const projectDir = process.cwd();
   if (fs.existsSync(path.join(projectDir, 'config.xml'))) return projectDir;
-  else return undefined;
+  else throw new Error('Could not find a cordova-project in current directory or provided path');
 };
 
 main(commandLineArgs(optionDefinitions));
